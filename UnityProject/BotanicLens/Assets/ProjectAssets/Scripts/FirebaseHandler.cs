7d09f4ef53cdbb10c779475dc3fc645fcfadf1b9
andreea-olariu <83142073+andreea-olariu@users.noreply.github.com>
2023-12-11 22:52:55 +0200

    db

--- UnityProject/BotanicLens/Assets/ProjectAssets/Scripts/FirebaseHandler.cs
+++ UnityProject/BotanicLens/Assets/ProjectAssets/Scripts/FirebaseHandler.cs
@@ -1,14 +1,59 @@
+
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Firebase;
 using Firebase.Database;
+using Firebase.Extensions;
 using UnityEngine;
 
-public class FirebaseHandler : MonoBehaviour
+public class FirebaseHandler
 {
-    DatabaseReference reference;
-
-    public void GetAllPlants()
+    private DatabaseReference databaseReference;
+    public List<string> plants;
+    public FirebaseHandler()
     {
+        plants = new List<string>();
+        InitializeFirebase();
+    }
 
+    private void InitializeFirebase()
+    {
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task =>
+        {
+            FirebaseApp app = FirebaseApp.DefaultInstance;
+            databaseReference = FirebaseDatabase.DefaultInstance.RootReference;
+            string collectionName = "inventory";
+            databaseReference.Child("inventory").Child("2343432").SetValueAsync("Cactus");
+            RetrieveCollectionElements(databaseReference.Child(collectionName));
+        });
+    }
+
+    private async void RetrieveCollectionElements(DatabaseReference collectionReference)
+    {
+        try
+        {
+            DataSnapshot snapshot = await collectionReference.GetValueAsync();
+
+            if (snapshot != null && snapshot.Exists)
+            {
+                foreach (var childSnapshot in snapshot.Children)
+                {
+                    string key = childSnapshot.Key;
+                    string value = childSnapshot.GetRawJsonValue();
+
+                    Debug.Log($"Key: {key}, Value: {value}");
+                    this.plants.Add(value);
+                }
+            }
+            else
+            {
+                Debug.Log("Collection is empty or does not exist.");
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.LogError($"Error retrieving collection elements: {e.Message}");
+        }
     }
 }
--- UnityProject/BotanicLens/Assets/ProjectAssets/Scripts/PopulateInventory.cs
+++ UnityProject/BotanicLens/Assets/ProjectAssets/Scripts/PopulateInventory.cs
@@ -7,25 +7,27 @@ using UnityEngine.UI;
 
 public class PopulateInventory : MonoBehaviour
 {
-    
+    FirebaseHandler handler;
+
     void Start()
     {
+        handler = new FirebaseHandler();
+        Debug.Log(handler.plants.Count);
+        for (int i = 0; i < handler.plants.Count; i++)
+        {
+            GameObject menuItem = new GameObject("MenuItem");
 
-        //for(int i = 0; i < plants.Count; i++)
-        //{
-        //    GameObject menuItem = new GameObject("MenuItem");
+            RectTransform menuItemRectTransform = menuItem.AddComponent<RectTransform>();
+            menuItemRectTransform.sizeDelta = new Vector2(200f + i * 30, 50f - i * 10);
+            menuItem.transform.SetParent(this.transform, false);
 
-        //    RectTransform menuItemRectTransform = menuItem.AddComponent<RectTransform>();
-        //    menuItemRectTransform.sizeDelta = new Vector2(200f + i * 30, 50f - i * 10);
-        //    menuItem.transform.SetParent(this.transform, false);
+            GameObject plantName = new GameObject(handler.plants[i]);
+            TextMeshProUGUI plantNameText = plantName.AddComponent<TextMeshProUGUI>();
+            plantName.transform.SetParent(menuItemRectTransform, false);
+            plantNameText.text = handler.plants[i];
 
-        //    GameObject plantName = new GameObject(plants[i]);
-        //    TextMeshProUGUI plantNameText = plantName.AddComponent<TextMeshProUGUI>();
-        //    plantName.transform.SetParent(menuItemRectTransform, false);
-        //    plantNameText.text = plants[i];
-
-        //    plantNameText.fontSize = 50;
-        //    plantNameText.color = Color.black;
-        //}
+            plantNameText.fontSize = 50;
+            plantNameText.color = Color.black;
+        }
     }
 }
